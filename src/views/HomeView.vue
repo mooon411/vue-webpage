4a5c599e6b8bc2d0b6090685bd4f869f18cbd9e1
mooon411 <duddls0411@gmail.com>
2023-10-31 11:07:35 +0900

    two
    반복문

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,18 +9,17 @@ import Banner from "@/components/Banner.vue"
 import Text from "@/components/Text.vue"
 import Footer from "@/components/Footer.vue"
 
-
 </script>
 
 <template>
-  <Header />
+  <Header element="nexon" />
   <main>
-    <Slider />
-    <Image />
-    <ImgText />
-    <Card />
-    <Banner />
-    <Text />
+    <Slider element="nexon" />
+    <Image element="nexon section" title="포트폴리오가 실력이다."/>
+    <ImgText element="nexon section gray" title='이미지 텍스트 유형' />
+    <Card element="nexon section" title='웹스토리보이 강의' />
+    <Banner element="nexon section"  title='배너 영역' />
+    <Text element="nexon section" title='텍스트 유형01' />
   </main>
-  <Footer />
+  <Footer element="nexon section gray" />
 </template>
